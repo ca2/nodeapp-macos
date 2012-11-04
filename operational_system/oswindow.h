acfd8363c7cea5fc42d305f031eeac185465c61f
ca2 <camilo@ca2.email>
2012-11-04 07:29:10 +0000

    porting to mac os Mountain Lion
    Eu te amo Thommy!!
    
    Mummi contribution!!! Feliz 61 anos!!
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++
    
    desobrigado.

---
+++ operational_system/oswindow.h
@@ -0,0 +1,120 @@
+//
+//  oswindow.h
+//  os
+//
+//  Created by Thomas Borregaard Sørensen on 11/4/12.
+//  Copyright (c) 2012 ca2 Desenvolvimento de Sofware Ltda. All rights reserved.
+//
+
+
+// backlink at inha-ubuntu at veriverse at Curitiba near Finados Holyday 2012-11-03 from ca.dylib/ca.so/ca.dll
+// and now on 2012-11-04, backlink at inha macos mountain lion
+namespace ca // Thommy Gustavinho Cecynzinho Lundgrenzinho
+{
+   
+   class CLASS_DECL_c null
+   {
+   public:
+      
+      
+      null() {}
+      
+      
+   };
+   
+} // namespace
+
+class oswindow_dataptra;
+
+#ifdef __MM
+typedef NSWindow * nswindow;
+#else
+typedef void * nswindow;
+#endif
+
+class CLASS_DECL_c oswindow
+{
+public:
+   
+   
+   class CLASS_DECL_c data
+   {
+   public:
+      
+      nswindow    m_nswindow;
+      
+   };
+   
+private:
+   
+   
+   
+   
+   
+   
+   data *   m_pdata;
+   
+   
+   static oswindow_dataptra * s_pdataptra;
+   static int find(nswindow window);
+   static data * get(nswindow window);
+   
+   
+   
+public:
+   
+   
+   oswindow();
+   oswindow(const ::ca::null & null);
+   oswindow(nswindow window);
+   oswindow(const oswindow & oswindow);
+   oswindow(const LPARAM & lparam);
+   oswindow(const WPARAM & wparam);
+   
+   
+   
+   operator void * ()
+   {
+      
+      return m_pdata;
+      
+   }
+   
+#ifdef __MM
+   
+   operator ::nswindow ()
+   {
+      return window();
+   }
+   
+#endif
+   
+   static bool remove(nswindow window);
+   
+   
+   oswindow & operator = (const oswindow & window);
+   
+   bool operator == (const void * p) const
+   {
+      return m_pdata == p;
+   }
+   bool operator != (const void * p) const
+   {
+      return m_pdata != p;
+   }
+   bool operator == (const oswindow & w) const
+   {
+      return m_pdata == w.m_pdata;
+   }
+   bool operator != (const oswindow & w) const
+   {
+      return m_pdata != w.m_pdata;
+   }
+   
+   nswindow window()
+   {
+      return m_pdata == NULL ? NULL : m_pdata->m_nswindow;
+   }
+   
+   
+};
