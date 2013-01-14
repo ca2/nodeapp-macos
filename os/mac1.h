2cfbd83aaf6728ae58fe734c33e236b9ac4b0992
ca2 <camilo@ca2.email>
2013-01-14 23:08:01 +0000

    Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- os/mac1.h
+++ os/mac1.h
@@ -1,32 +1,32 @@
 #pragma once
 
-namespace win
+namespace mac
 {
    class thread;
 
-   class win
+   class mac
    {
       int function();
    };
 
-   CLASS_DECL_VMSWIN HINSTANCE   LoadLibrary(const char * lpsz);
-   CLASS_DECL_VMSWIN BOOL        SHGetSpecialFolderPath(HWND hwnd, string &str, int csidl, BOOL fCreate);
-   CLASS_DECL_VMSWIN DWORD       GetFileAttributes(const char * lpFileName);
-   CLASS_DECL_VMSWIN BOOL        CreateDirectory(const char * lpPathName, LPSECURITY_ATTRIBUTES lpSecurityAttributes);
-   CLASS_DECL_VMSWIN DWORD       GetCurrentDirectory(string & str);
-   CLASS_DECL_VMSWIN DWORD       GetTempPath(string & str);
-   CLASS_DECL_VMSWIN LONG        RegQueryValue(HKEY hkey, const char * lpszSubKey, string & str);
-   CLASS_DECL_VMSWIN HICON       ExtractIcon(HINSTANCE hInst, const char * lpszExeFileName, UINT nIconIndex);
-   CLASS_DECL_VMSWIN BOOL        DeleteFile(const char * lpFileName);
-   CLASS_DECL_VMSWIN int         GetMenuStringW(HMENU hMenu, UINT uIDItem, string & str, UINT flags);
-   CLASS_DECL_VMSWIN void        TimeToFileTime(::ca::application * papp, const class time& time, LPFILETIME pFileTime);
+   CLASS_DECL_VMSMAC HINSTANCE   LoadLibrary(const char * lpsz);
+   CLASS_DECL_VMSMAC WINBOOL        SHGetSpecialFolderPath(oswindow hwnd, string &str, int csidl, WINBOOL fCreate);
+   CLASS_DECL_VMSMAC DWORD       GetFileAttributes(const char * lpFileName);
+   CLASS_DECL_VMSMAC WINBOOL        CreateDirectory(const char * lpPathName, LPSECURITY_ATTRIBUTES lpSecurityAttributes);
+   CLASS_DECL_VMSMAC DWORD       GetCurrentDirectory(string & str);
+   CLASS_DECL_VMSMAC DWORD       GetTempPath(string & str);
+//   CLASS_DECL_VMSMAC LONG        RegQueryValue(HKEY hkey, const char * lpszSubKey, string & str);
+   CLASS_DECL_VMSMAC HICON       ExtractIcon(HINSTANCE hInst, const char * lpszExeFileName, UINT nIconIndex);
+   CLASS_DECL_VMSMAC WINBOOL        DeleteFile(const char * lpFileName);
+   CLASS_DECL_VMSMAC int         GetMenuStringW(HMENU hMenu, UINT uIDItem, string & str, UINT flags);
+   CLASS_DECL_VMSMAC void        TimeToFileTime(::ca::application * papp, const class time& time, LPFILETIME pFileTime);
 
-} // namespace win
+} // namespace mac
 
 
 
 // Placed on frame for EXCEPTION linkage, or base_exception cleanup
-struct CLASS_DECL_VMSWIN AFX_EXCEPTION_LINK
+struct CLASS_DECL_VMSMAC AFX_EXCEPTION_LINK
 {
    AFX_EXCEPTION_LINK* m_pLinkPrev;    // previous top, next in handler chain
    base_exception* m_pException;   // current exception (NULL in try block)
@@ -37,7 +37,7 @@ struct CLASS_DECL_VMSWIN AFX_EXCEPTION_LINK
 };
 
 // Exception global state - never access directly
-struct CLASS_DECL_VMSWIN AFX_EXCEPTION_CONTEXT
+struct CLASS_DECL_VMSMAC AFX_EXCEPTION_CONTEXT
 {
    AFX_EXCEPTION_LINK* m_pLinkTop;
 
@@ -49,26 +49,26 @@ typedef int (__cdecl * _PNH)( size_t );
 #define _PNH_DEFINED
 #endif
 
-_PNH CLASS_DECL_VMSWIN AfxGetNewHandler();
-_PNH CLASS_DECL_VMSWIN AfxSetNewHandler(_PNH pfnNewHandler);
+_PNH CLASS_DECL_VMSMAC AfxGetNewHandler();
+_PNH CLASS_DECL_VMSMAC AfxSetNewHandler(_PNH pfnNewHandler);
 CLASS_DECL_ca int AFX_CDECL AfxNewHandler(size_t nSize);
 
-void CLASS_DECL_VMSWIN AfxAbort();
+void CLASS_DECL_VMSMAC AfxAbort();
 
 
 // helpers for registering your own WNDCLASSes
-CLASS_DECL_VMSWIN const char * AfxRegisterWndClass(UINT nClassStyle,
+CLASS_DECL_VMSMAC const char * AfxRegisterWndClass(UINT nClassStyle,
    HCURSOR hCursor = 0, HBRUSH hbrBackground = 0, HICON hIcon = 0);
 
-CLASS_DECL_VMSWIN BOOL AfxRegisterClass(WNDCLASS* lpWndClass);
+CLASS_DECL_VMSMAC WINBOOL AfxRegisterClass(WNDCLASS* lpWndClass);
 
 // helper to initialize rich edit 1.0 control
-CLASS_DECL_VMSWIN BOOL AfxInitRichEdit();
+CLASS_DECL_VMSMAC WINBOOL AfxInitRichEdit();
 // helper to initialize rich edit 2.0 control
-CLASS_DECL_VMSWIN BOOL AfxInitRichEdit2();
+CLASS_DECL_VMSMAC WINBOOL AfxInitRichEdit2();
 
-CLASS_DECL_VMSWIN LRESULT CALLBACK AfxWndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam);
-CLASS_DECL_VMSWIN WNDPROC AfxGetAfxWndProc();
+CLASS_DECL_VMSMAC LRESULT CALLBACK AfxWndProc(oswindow hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam);
+CLASS_DECL_VMSMAC WNDPROC AfxGetAfxWndProc();
 #define AfxWndProc (*AfxGetAfxWndProc())
 
 typedef void (AFX_MSG_CALL ::ca::window::*AFX_PMSGW)(void);
@@ -80,21 +80,21 @@ typedef void (AFX_MSG_CALL ::radix::thread::*AFX_PMSGT)(void);
 
 
 
-CLASS_DECL_VMSWIN LONG AfxDelRegTreeHelper(HKEY hParentKey, const string & strKeyName);
+CLASS_DECL_VMSMAC LONG AfxDelRegTreeHelper(HKEY hParentKey, const string & strKeyName);
 
 
 // Advanced initialization: for overriding default diagnostics
-CLASS_DECL_VMSWIN BOOL AfxDiagnosticInit(void);
+CLASS_DECL_VMSMAC WINBOOL AfxDiagnosticInit(void);
 
 
-CLASS_DECL_VMSWIN ::win::thread * AfxGetThread();
-CLASS_DECL_VMSWIN void AfxSetThread(::radix::thread * pthread);
-CLASS_DECL_VMSWIN MSG* AfxGetCurrentMessage();
+CLASS_DECL_VMSMAC ::mac::thread * AfxGetThread();
+CLASS_DECL_VMSMAC void AfxSetThread(::radix::thread * pthread);
+CLASS_DECL_VMSMAC MSG* AfxGetCurrentMessage();
 
-CLASS_DECL_VMSWIN void AfxEndThread(::radix::application * papp, UINT nExitCode, BOOL bDelete = TRUE);
+CLASS_DECL_VMSMAC void AfxEndThread(::radix::application * papp, UINT nExitCode, WINBOOL bDelete = TRUE);
 
-CLASS_DECL_VMSWIN void AfxInitThread();
-CLASS_DECL_VMSWIN void AfxTermThread(::radix::application * papp, HINSTANCE hInstTerm = NULL);
+CLASS_DECL_VMSMAC void AfxInitThread();
+CLASS_DECL_VMSMAC void AfxTermThread(::radix::application * papp, HINSTANCE hInstTerm = NULL);
 
 /////////////////////////////////////////////////////////////////////////////
 // Global functions for access to the one and only application
@@ -121,20 +121,20 @@ CLASS_DECL_VMSWIN void AfxTermThread(::radix::application * papp, HINSTANCE hIns
 #define afxCommDlgWrapper static_cast<CCommDlgWrapper*>(AfxGetModuleState()->m_pDllIsolationWrappers[_AFX_COMMDLG_ISOLATION_WRAPPER_INDEX])
 
 // Advanced initialization: for overriding default WinMain
-//CLASS_DECL_VMSWIN BOOL AfxWinInit(__in HINSTANCE hInstance, __in HINSTANCE hPrevInstance,
+//CLASS_DECL_VMSMAC WINBOOL AfxWinInit(__in HINSTANCE hInstance, __in HINSTANCE hPrevInstance,
   // __in_z LPTSTR lpCmdLine, __in int nCmdShow);
-CLASS_DECL_VMSWIN void AfxWinTerm();
+CLASS_DECL_VMSMAC void AfxWinTerm();
 
-CLASS_DECL_VMSWIN ::radix::application* AfxGetApp();
-CLASS_DECL_VMSWIN ::user::interaction* AfxGetMainWnd();
-//CLASS_DECL_VMSWIN HINSTANCE CLASS_DECL_VMSWIN System.m_hInstance;
-CLASS_DECL_VMSWIN HINSTANCE AfxGetResourceHandle();
-CLASS_DECL_VMSWIN void AfxSetResourceHandle(HINSTANCE hInstResource);
-CLASS_DECL_VMSWIN const char * AfxGetAppName();
+CLASS_DECL_VMSMAC ::radix::application* AfxGetApp();
+CLASS_DECL_VMSMAC ::user::interaction* AfxGetMainWnd();
+//CLASS_DECL_VMSMAC HINSTANCE CLASS_DECL_VMSMAC System.m_hInstance;
+CLASS_DECL_VMSMAC HINSTANCE AfxGetResourceHandle();
+CLASS_DECL_VMSMAC void AfxSetResourceHandle(HINSTANCE hInstResource);
+CLASS_DECL_VMSMAC const char * AfxGetAppName();
 
-CLASS_DECL_VMSWIN HINSTANCE AfxGetResourceHandle();
-CLASS_DECL_VMSWIN HINSTANCE AfxFindStringResourceHandle(UINT nID);
+CLASS_DECL_VMSMAC HINSTANCE AfxGetResourceHandle();
+CLASS_DECL_VMSMAC HINSTANCE AfxFindStringResourceHandle(UINT nID);
 
-CLASS_DECL_VMSWIN void AfxDeleteObject(HGDIOBJ* pObject);
+CLASS_DECL_VMSMAC void AfxDeleteObject(HGDIOBJ* pObject);
 
-void CLASS_DECL_VMSWIN AfxGlobalFree(HGLOBAL hGlobal);
+void CLASS_DECL_VMSMAC AfxGlobalFree(HGLOBAL hGlobal);
