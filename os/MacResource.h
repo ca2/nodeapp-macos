e05fb6a497199712a1d0be2c4b273dc74b527fd2
ca2 <camilo@ca2.email>
2013-01-14 23:27:15 +0000

    Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- os/MacResource.h
+++ os/MacResource.h
@@ -1,10 +1,10 @@
 #pragma once
 
 
-CLASS_DECL_VMSWIN HINSTANCE  vfxFindResourceHandle(const char * lpszName, const char * lpszType);
-CLASS_DECL_VMSWIN HINSTANCE  vfxFindResourceHandle(const char * lpszName, const char * lpszType);
+CLASS_DECL_VMSMAC HINSTANCE  vfxFindResourceHandle(const char * lpszName, const char * lpszType);
+CLASS_DECL_VMSMAC HINSTANCE  vfxFindResourceHandle(const char * lpszName, const char * lpszType);
 
-class CLASS_DECL_VMSWIN WinResource :
+class CLASS_DECL_VMSMAC WinResource :
    virtual public ex1::resource
 {
 public:
