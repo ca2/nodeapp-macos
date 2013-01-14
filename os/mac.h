788450f6a5d8f1d7b1cba8144aae0c5de1df9520
ca2 <camilo@ca2.email>
2013-01-14 23:05:52 +0000

    Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- os/mac.h
+++ os/mac.h
@@ -1,23 +1,14 @@
 #pragma once
 
+
 #define SECURITY_WIN32
 
-#include "ca/ca.h"
-#include <shlobj.h>
-#include <Security.h>
-#include <shlobj.h>
+
+#include "app/appseed/ca/ca.h"
 
 
+#define CLASS_DECL_VMSMAC
 
-
-
-
-
-#ifdef _VMSWIN_DLL
-    #define CLASS_DECL_VMSWIN  _declspec(dllexport)
-#else
-    #define CLASS_DECL_VMSWIN  _declspec(dllimport)
-#endif
 
 string get_error_message(DWORD dwError);
 
@@ -26,42 +17,42 @@ string get_error_message(DWORD dwError);
 /////////////////////////////////////////////////////////////////////////////
 // explicit initialization for general purpose classes
 
-CLASS_DECL_VMSWIN WINBOOL AfxInitialize(WINBOOL bDLL = FALSE, DWORD dwVersion = _MFC_VER);
+CLASS_DECL_VMSMAC WINBOOL AfxInitialize(WINBOOL bDLL = FALSE, DWORD dwVersion = _MFC_VER);
 
 /////////////////////////////////////////////////////////////////////////////
 // stop on a specific primitive::memory request
 
 // Debugger hook on specified allocation request - Obsolete
-CLASS_DECL_VMSWIN void AfxSetAllocStop(LONG lRequestNumber);
+CLASS_DECL_VMSMAC void AfxSetAllocStop(LONG lRequestNumber);
 
 // Return TRUE if primitive::memory is sane or print out what is wrong
-CLASS_DECL_VMSWIN WINBOOL AfxCheckMemory();
+CLASS_DECL_VMSMAC WINBOOL AfxCheckMemory();
 
 // Return TRUE if valid primitive::memory block of nBytes
-CLASS_DECL_VMSWIN WINBOOL AfxIsMemoryBlock(const void * p, UINT nBytes,
+CLASS_DECL_VMSMAC WINBOOL AfxIsMemoryBlock(const void * p, UINT nBytes,
    LONG* plRequestNumber = NULL);
 
 // helper routines for non-C++ EH implementations
 // for THROW_LAST auto-delete backward compatiblity
-CLASS_DECL_VMSWIN void AfxThrowLastCleanup();
+CLASS_DECL_VMSMAC void AfxThrowLastCleanup();
 
 // other out-of-line helper functions
-CLASS_DECL_VMSWIN void AfxTryCleanup();
+CLASS_DECL_VMSMAC void AfxTryCleanup();
 
 
 /////////////////////////////////////////////////////////////////////////////
 // Global implementation helpers
 
 // window creation hooking
-CLASS_DECL_VMSWIN void AfxHookWindowCreate(::user::interaction * pWnd);
-CLASS_DECL_VMSWIN WINBOOL AfxUnhookWindowCreate();
-CLASS_DECL_VMSWIN void AfxResetMsgCache();
+CLASS_DECL_VMSMAC void AfxHookWindowCreate(::user::interaction * pWnd);
+CLASS_DECL_VMSMAC WINBOOL AfxUnhookWindowCreate();
+CLASS_DECL_VMSMAC void AfxResetMsgCache();
 
 // for backward compatibility to previous versions
 #define _AfxHookWindowCreate    AfxHookWindowCreate
 #define _AfxUnhookWindowCreate  AfxUnhookWindowCreate
 
-#include "win1.h"
+#include "mac1.h"
 #include "implementation.h"
 #include "state.h"
 #include "handle.h"
@@ -86,16 +77,16 @@ CLASS_DECL_VMSWIN void AfxResetMsgCache();
 #include "port_forward.h"
 
 #define NULL_REF(class) (*((class *) NULL))
-CLASS_DECL_VMSWIN WNDPROC AfxGetAfxWndProc();
+CLASS_DECL_VMSMAC WNDPROC AfxGetAfxWndProc();
 #define AfxWndProc (*AfxGetAfxWndProc())
 
-#define WIN_THREAD(pthread) (dynamic_cast < ::win::thread * > (dynamic_cast < ::ca::thread * >(pthread)))
-#define WIN_WINDOW(pwnd) (dynamic_cast < ::win::window * > (dynamic_cast < ::ca::window * >(pwnd)))
-#define WIN_DC(pgraphics) (dynamic_cast < ::win::graphics * > (dynamic_cast < ::ca::graphics * > (pgraphics)))
-#define SP_DC(pgraphics) (dynamic_cast < ::win::graphics * > (( ::ca::graphics * )(pgraphics)))
-#define WIN_HDC(pgraphics) ((HDC)*(dynamic_cast < ::win::graphics * > (dynamic_cast < ::ca::graphics * > (pgraphics))))
-#define SP_HDC(pgraphics) ((HDC)*(dynamic_cast < ::win::graphics * > ((::ca::graphics *)(pgraphics))))
-#define WIN_DIB(pdib) (dynamic_cast < ::win::dib * > (dynamic_cast < ::ca::dib * >(pdib)))
+#define WIN_THREAD(pthread) (dynamic_cast < ::mac::thread * > (dynamic_cast < ::ca::thread * >(pthread)))
+#define WIN_WINDOW(pwnd) (dynamic_cast < ::mac::window * > (dynamic_cast < ::ca::window * >(pwnd)))
+#define WIN_DC(pgraphics) (dynamic_cast < ::mac::graphics * > (dynamic_cast < ::ca::graphics * > (pgraphics)))
+#define SP_DC(pgraphics) (dynamic_cast < ::mac::graphics * > (( ::ca::graphics * )(pgraphics)))
+#define WIN_HDC(pgraphics) ((HDC)*(dynamic_cast < ::mac::graphics * > (dynamic_cast < ::ca::graphics * > (pgraphics))))
+#define SP_HDC(pgraphics) ((HDC)*(dynamic_cast < ::mac::graphics * > ((::ca::graphics *)(pgraphics))))
+#define WIN_DIB(pdib) (dynamic_cast < ::mac::dib * > (dynamic_cast < ::ca::dib * >(pdib)))
 
 
 #pragma comment(lib, "kernel32.lib")
@@ -113,15 +104,15 @@ CLASS_DECL_VMSWIN WNDPROC AfxGetAfxWndProc();
 #pragma comment(lib, "Msimg32.lib") 
 #pragma comment(lib, "Psapi.lib") 
 
-CLASS_DECL_VMSWIN void _AfxTraceMsg(const char * lpszPrefix, gen::signal_object * pobj);
-CLASS_DECL_VMSWIN void _AfxTraceMsg(const char * lpszPrefix, LPMSG lpmsg);
+CLASS_DECL_VMSMAC void _AfxTraceMsg(const char * lpszPrefix, gen::signal_object * pobj);
+CLASS_DECL_VMSMAC void _AfxTraceMsg(const char * lpszPrefix, LPMSG lpmsg);
 
-CLASS_DECL_VMSWIN WINBOOL __cdecl AfxIsIdleMessage(gen::signal_object * pobj);
-CLASS_DECL_VMSWIN WINBOOL __cdecl AfxIsIdleMessage(MSG* pMsg);
+CLASS_DECL_VMSMAC WINBOOL __cdecl AfxIsIdleMessage(gen::signal_object * pobj);
+CLASS_DECL_VMSMAC WINBOOL __cdecl AfxIsIdleMessage(MSG* pMsg);
 
 
-CLASS_DECL_VMSWIN void AfxProcessWndProcException(base_exception*, gen::signal_object * pobj);
-CLASS_DECL_VMSWIN void __cdecl AfxPreTranslateMessage(gen::signal_object * pobj);
+CLASS_DECL_VMSMAC void AfxProcessWndProcException(base_exception*, gen::signal_object * pobj);
+CLASS_DECL_VMSMAC void __cdecl AfxPreTranslateMessage(gen::signal_object * pobj);
 
 
 #include "application.h"
