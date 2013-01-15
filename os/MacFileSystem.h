950ff581001ed59bfb929b293f234207e18b95a3
ca2 <camilo@ca2.email>
2013-01-15 19:28:27 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy  Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- os/MacFileSystem.h
+++ os/MacFileSystem.h
@@ -1,19 +1,36 @@
 #pragma once
 
-CLASS_DECL_VMSMAC WINBOOL vfxFullPath(wchar_t * lpszPathOut, const wchar_t * lpszFileIn);
-CLASS_DECL_VMSMAC UINT vfxGetFileName(const wchar_t * lpszPathName, wchar_t * lpszTitle, UINT nMax);
-CLASS_DECL_VMSMAC void vfxGetModuleShortFileName(HINSTANCE hInst, string & strShortName);
-CLASS_DECL_VMSMAC void vfxGetModuleShortFileName(HINSTANCE hInst, string& strShortName);
 
-class WinFileSystem :
-   virtual public ex1::file_system
+//CLASS_DECL_mac WINBOOL vfxFullPath(wchar_t * lpszPathOut, const wchar_t * lpszFileIn);
+//CLASS_DECL_mac UINT vfxGetFileName(const wchar_t * lpszPathName, wchar_t * lpszTitle, UINT nMax);
+//CLASS_DECL_mac void vfxGetModuleShortFileName(HINSTANCE hInst, string & strShortName);
+//CLASS_DECL_mac void vfxGetModuleShortFileName(HINSTANCE hInst, string& strShortName);
+
+
+namespace mac
 {
-public:
-   WinFileSystem(::ca::application * papp);
-   virtual ~WinFileSystem();
+   
+   
+   class file_system :
+   virtual public ex1::file_system
+   {
+   public:
+      
+      
+      file_system(::ca::application * papp);
+      virtual ~file_system();
+      
+      
+      //    virtual bool FullPath(string & str, const char * lpszFileIn);
+      //    virtual UINT GetFileName(const char * lpszPathName, string & str);
+      //    virtual void GetModuleShortFileName(HINSTANCE hInst, string & strShortName);
+      
+      
+   };
+   
+   
+} // namespace mac
 
-   virtual bool FullPath(string & str, const char * lpszFileIn);
-   virtual UINT GetFileName(const char * lpszPathName, string & str);
-   virtual void GetModuleShortFileName(HINSTANCE hInst, string & strShortName);
 
-};
+
+
