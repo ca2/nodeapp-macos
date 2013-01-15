711ff72c76ba1510a1081adacc5227006cca5b37
ca2 <camilo@ca2.email>
2013-01-15 18:57:32 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy  Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- os/genResource.cpp
+++ os/genResource.cpp
@@ -78,7 +78,7 @@ namespace gen
 } //namespace gen
 
 
-HINSTANCE CLASS_DECL_VMSMAC vfxFindResourceHandle(const char * lpszName, const char * lpszType)
+HINSTANCE CLASS_DECL_mac vfxFindResourceHandle(const char * lpszName, const char * lpszType)
 {
    ASSERT(lpszName != NULL);
    ASSERT(lpszType != NULL);
@@ -140,7 +140,7 @@ HINSTANCE CLASS_DECL_VMSMAC vfxFindResourceHandle(const char * lpszName, const c
    return System.m_hInstance;
 }
 
-HINSTANCE CLASS_DECL_VMSMAC vfxFindResourceHandle(const wchar_t * lpszName, const wchar_t * lpszType)
+HINSTANCE CLASS_DECL_mac vfxFindResourceHandle(const wchar_t * lpszName, const wchar_t * lpszType)
 {
    ASSERT(lpszName != NULL);
    ASSERT(lpszType != NULL);
