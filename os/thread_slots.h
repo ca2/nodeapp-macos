e96e88b28d5ac2a580eb27160b4d4b8e6c597dbe
ca2 <camilo@ca2.email>
2013-01-15 20:21:44 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy  Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- os/thread_slots.h
+++ os/thread_slots.h
@@ -1,13 +1,11 @@
-
-#ifndef __AFXTLS_H__
-#define __AFXTLS_H__
-
 #pragma once
+
 
 // Classes declared in this file
 
-class simple_list;
+//class simple_list;
 class thread_slot_data;                  // for manipulationg thread local storage
+template < int32_t iSlot >
 class thread_local_object;               // for storing thread local data
 class process_local_object;              // for storing thread local data
 class no_track_object;
@@ -22,119 +20,181 @@ class no_track_object;
 struct thread_data; // private to implementation
 struct slot_data;   // private to implementation
 
-class CLASS_DECL_VMSMAC thread_slot_data
+
+class CLASS_DECL_mac thread_slot_data
 {
 public:
+   
+   
+   no_track_object *                           m_pa[1024]; // state of each slot (allocated or not)
+   
+   
    thread_slot_data();
-
-// Operations
-   int AllocSlot();
-   void FreeSlot(int nSlot);   
-   void SetValue(int nSlot, void * pValue);
-   // delete all values in process/thread
-   void DeleteValues(HINSTANCE hInst, WINBOOL bAll = FALSE);
-   // assign instance handle to just constructed slots
-   void AssignInstance(HINSTANCE hInst);
-
-// Implementation
-   DWORD m_tlsIndex;   // used to access system thread-local storage
-
-   int m_nAlloc;       // number of slots allocated (in UINTs)
-   int m_nRover;       // (optimization) for quick finding of free slots
-   int m_nMax;         // size of slot table below (in bits)
-   slot_data* m_pSlotData; // state of each slot (allocated or not)
-   typed_simple_list<thread_data*> m_list;  // list of thread_data structures
-   CRITICAL_SECTION m_sect;
-
-   void * GetThreadValue(int nSlot); // special version for threads only!
-#undef new
-   void * PASCAL operator new(size_t, void * p)
-      { return p; }
-#define new DEBUG_NEW
-   void DeleteValues(thread_data* pData, HINSTANCE hInst);
-   ~thread_slot_data();
+   void delete_data();
+   
 };
 
-class CLASS_DECL_VMSMAC AFX_NOVTABLE no_track_object
+
+class CLASS_DECL_mac thread_local_storage
+{
+public:
+   
+   pthread_key_t key;   // used to access system thread-local storage
+   thread_slot_data *               m_pthreadslotdata;
+   
+   
+   thread_local_storage();
+   ~thread_local_storage();
+   void delete_data();
+   
+   thread_slot_data * get_slot_data();
+#undef new
+   void * PASCAL operator new(size_t, void * p)
+   { return p; }
+#define new DEBUG_NEW
+};
+
+
+extern BYTE _gen_ThreadData[sizeof(thread_local_storage)];
+extern thread_local_storage * __thread_data;
+
+
+class CLASS_DECL_mac no_track_object
 {
 public:
 #undef new
    void * PASCAL operator new(size_t nSize);
 #define new DEBUG_NEW
    void PASCAL operator delete(void *);
-
-#if defined(_DEBUG) && !defined(_AFX_NO_DEBUG_CRT)
+   
+#if defined(DEBUG) && !defined(___NO_DEBUG_CRT)
 #undef new
-   void * PASCAL operator new(size_t nSize, const char *, int);
+   void * PASCAL operator new(size_t nSize, const char *, int32_t);
 #define new DEBUG_NEW
-   void PASCAL operator delete(void * pObject, const char *, int);
+   void PASCAL operator delete(void * pObject, const char *, int32_t);
 #endif
-    virtual ~no_track_object() {};
+   virtual ~no_track_object() {};
 };
 
-class CLASS_DECL_VMSMAC AFX_NOVTABLE thread_local_object
+template < int32_t iSlot >
+class thread_local_object
 {
 public:
-// Attributes
+   // Attributes
    no_track_object* get_data(no_track_object* ( * pfnCreateObject)());
    no_track_object* GetDataNA();
-
-// Implementation
-   int m_nSlot;
+   
+   // Implementation
+   //int32_t m_nSlot;
    ~thread_local_object();
 };
 
-class CLASS_DECL_VMSMAC AFX_NOVTABLE process_local_object
+
+template < int32_t iSlot >
+no_track_object* thread_local_object < iSlot> ::get_data(no_track_object* ( * pfnCreateObject)())
+{
+   
+   if (__thread_data == NULL)
+   {
+#undef new
+      __thread_data = new(_gen_ThreadData) thread_local_storage;
+#define new DEBUG_NEW
+      ENSURE(__thread_data != NULL);
+   }
+   
+   no_track_object * pValue = (no_track_object *) __thread_data->get_slot_data()->m_pa[iSlot];
+   
+   if(pValue == NULL)
+   {
+      
+      // allocate zero-init object
+      pValue =  (*pfnCreateObject)();
+      
+      // set tls data to newly created object
+      __thread_data->get_slot_data()->m_pa[iSlot] = pValue;
+      ASSERT(__thread_data->get_slot_data()->m_pa[iSlot] == pValue);
+      
+   }
+   
+   return pValue;
+   
+}
+template < int32_t iSlot >
+no_track_object* thread_local_object < iSlot > ::GetDataNA()
+{
+   return __thread_data->get_slot_data()->m_pa[iSlot];
+}
+template < int32_t iSlot >
+thread_local_object < iSlot > ::~thread_local_object()
+{
+   //   if (m_nSlot != 0 && __thread_data != NULL)
+   //    __thread_data->FreeSlot(m_nSlot);
+   //m_nSlot = 0;
+}
+
+
+class CLASS_DECL_mac process_local_object
 {
 public:
-// Attributes
-   no_track_object* get_data(no_track_object* (* pfnCreateObject)());
-
-// Implementation
-   no_track_object* volatile m_pObject;
+   
+   
+   mutex                         m_mutex;
+   no_track_object * volatile    m_pObject;
+   
+   
+   // Implementation
+   process_local_object() : m_mutex(NULL) { }
    ~process_local_object();
+   
+   
+   // Attributes
+   no_track_object* get_data(no_track_object* (* pfnCreateObject)());
+   
+   
 };
 
-template<class TYPE>
-class thread_local : public thread_local_object
+
+template < class TYPE, int32_t iSlot >
+class thread_local :
+public thread_local_object < iSlot >
 {
-// Attributes
+   // Attributes
 public:
    inline TYPE* get_data()
    {
-      TYPE* pData = (TYPE*)thread_local_object::get_data(&CreateObject);
-      ENSURE(pData != NULL);
-      return pData;
+      return (TYPE *) thread_local_object < iSlot >::get_data(&CreateObject);
+      //ENSURE(pData != NULL);
+      //return pData;
    }
    inline TYPE* GetDataNA()
    {
-      TYPE* pData = (TYPE*)thread_local_object::GetDataNA();
+      TYPE* pData = (TYPE*)thread_local_object < iSlot >::GetDataNA();
       return pData;
    }
    inline operator TYPE*()
-   { 
-      return get_data(); 
+   {
+      return get_data();
    }
    inline TYPE* operator->()
-   { 
-      return get_data(); 
+   {
+      return get_data();
    }
-
-// Implementation
+   
+   // Implementation
 public:
    static no_track_object* CreateObject()
-      { return new TYPE; }
+   { return new TYPE; }
 };
 
-#define THREAD_LOCAL(class_name, ident_name) \
-   AFX_COMDAT thread_local<class_name> ident_name;
-#define EXTERN_THREAD_LOCAL(class_name, ident_name) \
-   extern CLASS_DECL_VMSMAC thread_local<class_name> ident_name;
+#define THREAD_LOCAL(class_name, ident_name, slot) \
+thread_local < class_name, slot > ident_name;
+#define EXTERN_THREAD_LOCAL(class_name, ident_name, slot) \
+extern CLASS_DECL_mac thread_local<class_name, slot> ident_name;
 
 template<class TYPE>
 class process_local : public process_local_object
 {
-// Attributes
+   // Attributes
 public:
    inline TYPE* get_data()
    {
@@ -143,30 +203,27 @@ public:
       return pData;
    }
    inline TYPE* GetDataNA()
-      { return (TYPE*)m_pObject; }
+   { return (TYPE*)m_pObject; }
    inline operator TYPE*()
-      { return get_data(); }
+   { return get_data(); }
    inline TYPE* operator->()
-      { return get_data(); }
-
-// Implementation
+   { return get_data(); }
+   
+   // Implementation
 public:
    static no_track_object* CreateObject()
-      { return new TYPE; }
+   { return new TYPE; }
 };
 
 #define PROCESS_LOCAL(class_name, ident_name) \
-   AFX_COMDAT process_local<class_name> ident_name;
+process_local < class_name > ident_name;
 #define EXTERN_PROCESS_LOCAL(class_name, ident_name) \
-   extern process_local<class_name> ident_name;
+extern process_local < class_name > ident_name;
 
 /////////////////////////////////////////////////////////////////////////////
 
-void CLASS_DECL_VMSMAC AfxInitLocalData(HINSTANCE hInstInit);
-void CLASS_DECL_VMSMAC AfxTermLocalData(HINSTANCE hInstTerm, WINBOOL bAll = FALSE);
-void CLASS_DECL_VMSMAC AfxTlsAddRef();
-void CLASS_DECL_VMSMAC AfxTlsRelease();
+void CLASS_DECL_mac __init_local_data(HINSTANCE hInstInit);
+void CLASS_DECL_mac __term_local_data(HINSTANCE hInstTerm, bool bAll = FALSE);
+void CLASS_DECL_mac __tls_add_ref();
+void CLASS_DECL_mac __tls_release();
 
-#endif //__AFXTLS_H__
-
-/////////////////////////////////////////////////////////////////////////////
