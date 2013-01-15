045d6a3277ea62b5040e3c54f89fe1173d2fe0c3
ca2 <camilo@ca2.email>
2013-01-15 20:20:50 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy  Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- os/thread_local_storage.cpp
+++ os/thread_local_storage.cpp
@@ -432,13 +432,13 @@ process_local_object::~process_local_object()
 /////////////////////////////////////////////////////////////////////////////
 // Init/Term for thread/process local data
 
-void CLASS_DECL_VMSMAC AfxInitLocalData(HINSTANCE hInst)
+void CLASS_DECL_mac AfxInitLocalData(HINSTANCE hInst)
 {
    if (_afxThreadData != NULL)
       _afxThreadData->AssignInstance(hInst);
 }
 
-void CLASS_DECL_VMSMAC AfxTermLocalData(HINSTANCE hInst, WINBOOL bAll)
+void CLASS_DECL_mac AfxTermLocalData(HINSTANCE hInst, WINBOOL bAll)
 {
    if (_afxThreadData != NULL)
       _afxThreadData->DeleteValues(hInst, bAll);
@@ -451,12 +451,12 @@ void CLASS_DECL_VMSMAC AfxTermLocalData(HINSTANCE hInst, WINBOOL bAll)
 
 __STATIC_DATA long _afxTlsRef = 0;
 
-void CLASS_DECL_VMSMAC AfxTlsAddRef()
+void CLASS_DECL_mac AfxTlsAddRef()
 {
    ++_afxTlsRef;
 }
 
-void CLASS_DECL_VMSMAC AfxTlsRelease()
+void CLASS_DECL_mac AfxTlsRelease()
 {
    if (_afxTlsRef == 0 || --_afxTlsRef == 0)
    {
