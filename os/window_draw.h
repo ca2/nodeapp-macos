7b6d7ae4a29ae42ce23d1f0272a7784d841539c1
ca2 <camilo@ca2.email>
2013-01-15 20:34:00 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy  Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- os/window_draw.h
+++ os/window_draw.h
@@ -1,29 +1,25 @@
 #pragma once
 
-namespace user
-{
-   class ::user::window_interface;
-} // namespace user
 
 namespace mac
 {
-
-   class CLASS_DECL_VMSMAC window_draw : 
-      virtual public ::ca::window_draw,
-      virtual public ::ca::message_window_simple_callback
+   
+   
+   class CLASS_DECL_mac window_draw :
+   virtual public ::ca::window_draw,
+   virtual public ::ca::message_window_simple_callback
    {
    public:
-
-
+      
+      
       enum EOptimize
       {
          OptimizeNone,
          OptimizeThis,
          OptimizeAllNext,
       };
-
-      user::LPWndArray                       m_wndpaOut;
-      user::window_draw_client_tool          m_twrenderclienttool;
+      
+      user::interaction_ptr_array                       m_wndpaOut;
       mutex                                  m_mutexRendering;
       mutex                                  m_mutexRgnUpdate;
       semaphore                              m_semaphoreBuffer;
@@ -32,117 +28,115 @@ namespace mac
       mutex                                  m_mutexRender;
       critical_section                       m_csWndInterfaceMap;
       typed_pointer_map <
-         CMapPtrToPtr,
-         oswindow, 
-         ::user::window_interface *>
-                                             m_wndinterfacemap;
+      map_ptr_to_ptr,
+      void *,
+      ::user::window_interface *>
+      m_wndinterfacemap;
       DWORD                                  m_dwLastUpdate;
-
-
+      DWORD                                  m_dwLastDelay;
+      
+      
       window_draw(::ca::application * papp);
       virtual ~window_draw();
-
-
+      
+      
       virtual bool start();
-
+      
       bool UpdateBuffer(LPCRECT lpcrect);
-      //void OnPaint(oswindow hwnd, CPaintDC & spgraphics);
-
+      //void OnPaint(void * hwnd, CPaintDC & spgraphics);
+      
       semaphore * TwfGetBufferSemaphore();
-
+      
       // Bit blitting spread functions
       virtual bool ScreenOutput(
-         user::buffer *,
-         ::ca::rgn & rgnUpdate);
-
+                                user::buffer *,
+                                ::ca::region & rgnUpdate);
+      
       virtual bool ScreenOutput();
-
-      virtual bool ScreenOutput(
-         user::buffer *,
-         ::user::interaction* pwnd,
-         ::ca::rgn & rgnUpdate);
-
+      
+      virtual bool ScreenOutput(user::buffer *, ::user::interaction* pwnd);
+      
       // Transparent drawing operations
       // controllers
       virtual bool UpdateBuffer();
-
+      
       virtual bool to(
-         ::ca::graphics *       pdc,
-         LPCRECT     lprectUpdate,
-         user::HwndTree & hwndtree,
-         bool        bGdiLocked,
-         bool        bExcludeParamWnd);
-
+                      ::ca::graphics *       pdc,
+                      LPCRECT     lprectUpdate,
+                      user::oswindow_tree & hwndtree,
+                      bool        bGdiLocked,
+                      bool        bExcludeParamWnd);
+      
       virtual bool to(
-         ::ca::graphics *       pdc,
-         LPCRECT     lprectUpdate,
-         user::HwndTree::Array & hwndtreea,
-         bool        bGdiLocked,
-         bool        bExcludeParamWnd);
-
-   /*     EOptimize TwfOptimizeRender2(
-         user::HwndTree::Array & hwndtreea,
-         LPCRECT lpcrect);
-
-      EOptimize TwfOptimizeRender2(
-         user::HwndTree & hwndtree,
-         LPCRECT lpcrect);*/
-
-
+                      ::ca::graphics *       pdc,
+                      LPCRECT     lprectUpdate,
+                      user::oswindow_tree::Array & hwndtreea,
+                      bool        bGdiLocked,
+                      bool        bExcludeParamWnd);
+      
+      /*     EOptimize TwfOptimizeRender2(
+       user::oswindow_tree::Array & hwndtreea,
+       LPCRECT lpcrect);
+       
+       EOptimize TwfOptimizeRender2(
+       user::oswindow_tree & hwndtree,
+       LPCRECT lpcrect);*/
+      
+      
       EOptimize TwfOptimizeRender(
-         user::HwndTree::Array & hwndtreea,
-         LPCRECT lpcrect);
-
+                                  user::oswindow_tree::Array & hwndtreea,
+                                  LPCRECT lpcrect);
+      
       EOptimize TwfOptimizeRender(
-         user::HwndTree & hwndtree,
-         LPCRECT lpcrect);
-
+                                  user::oswindow_tree & hwndtree,
+                                  LPCRECT lpcrect);
+      
       void TwfGetTopWindow(
-         oswindow hwnd,
-         user::HWNDArray & hwnda,
-         base_array < HRGN, HRGN > & hrgna,
-         user::HwndTree::Array & hwndtreea,
-         LPCRECT lpcrect);
-
+                           void * hwnd,
+                           user::oswindow_array & hwnda,
+                           base_array < HRGN, HRGN > & hrgna,
+                           user::oswindow_tree::Array & hwndtreea,
+                           LPCRECT lpcrect);
+      
       bool TwfGetTopWindow(
-         oswindow hwnd,
-         user::HWNDArray & hwnda,
-         base_array < HRGN, HRGN > & hrgna,
-         user::HwndTree::Array & hwndtreea,
-         HRGN hrgn);
-
+                           void * hwnd,
+                           user::oswindow_array & hwnda,
+                           base_array < HRGN, HRGN > & hrgna,
+                           user::oswindow_tree::Array & hwndtreea,
+                           HRGN hrgn);
+      
       bool TwfGetTopWindow(
-         oswindow hwnd,
-         user::HWNDArray & hwnda,
-         base_array < HRGN, HRGN > & hrgna,
-         user::HwndTree & hwndtree,
-         HRGN hrgn);
-
+                           void * hwnd,
+                           user::oswindow_array & hwnda,
+                           base_array < HRGN, HRGN > & hrgna,
+                           user::oswindow_tree & hwndtree,
+                           HRGN hrgn);
+      
       void TwfGetTopWindowOptimizeOpaque(
-         oswindow hwndOpaque,
-         user::HWNDArray & hwnda,
-         base_array < HRGN, HRGN > & hrgna);
-
-
+                                         void * hwndOpaque,
+                                         user::oswindow_array & hwnda,
+                                         base_array < HRGN, HRGN > & hrgna);
+      
+      
       EOptimize TwfOptimizeRenderRemoveNextProper(
-         user::HwndTree::Array & hwndtreea,
-         int iIndex,
-         LPCRECT lpcrect);
-
-      void get_wnda(user::LPWndArray & wndpa);
-      void get_wnda(user::HWNDArray & hwnda);
+                                                  user::oswindow_tree::Array & hwndtreea,
+                                                  int32_t iIndex,
+                                                  LPCRECT lpcrect);
+      
+      void get_wnda(user::interaction_ptr_array & wndpa);
+      void get_wnda(user::oswindow_array & hwnda);
       
       static UINT c_cdecl ThreadProcRedraw(LPVOID lpv);
-
+      
       virtual UINT RedrawProc();
-
+      
       virtual void asynch_redraw();
       virtual void _asynch_redraw();
       virtual void synch_redraw();
       virtual void _synch_redraw();
-
+      
       virtual void message_window_message_handler(gen::signal_object * pobj);
-
+      
    };
-
+   
 } // namespace mac
