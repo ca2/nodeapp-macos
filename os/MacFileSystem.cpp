c805a987d91396588a8681746d742e9ccf14da05
ca2 <camilo@ca2.email>
2013-01-15 19:27:15 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy  Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    
    -----8nnnnnnnn8
    ++++++++++

--- os/MacFileSystem.cpp
+++ os/MacFileSystem.cpp
@@ -1,58 +1,75 @@
-#include "StdAfx.h"
-#include "WinFileSystem.h"
+#include "framework.h"
 
 
-#include "WinFile.h"
 
-WinFileSystem::WinFileSystem(::ca::application * papp) :
+namespace mac
+{
+   
+   
+   file_system::file_system(::ca::application * papp) :
    ca(papp)
-{
-
-}
-
-WinFileSystem::~WinFileSystem()
-{
-
-}
-
-bool WinFileSystem::FullPath(string &str, const char * lpszFileIn)
-{
-   if(::ex1::file_system::FullPath(str, lpszFileIn))
-      return true;
-   if(gen::str::begins_ci(lpszFileIn, "http://"))
    {
-      str = lpszFileIn;
-      return true;
+      
    }
-   else if(gen::str::begins_ci(lpszFileIn, "https://"))
+   
+   
+   file_system::~file_system()
    {
-      str = lpszFileIn;
-      return true;
+      
    }
-   wstring wstrFileIn;
-   wstrFileIn = gen::international::utf8_to_unicode(lpszFileIn);
-   wstring wstrFileOut;
-   bool b = vfxFullPath(wstrFileOut.alloc(MAX_PATH * 8), wstrFileIn) != FALSE;
-   if(b)
-   {
-      gen::international::unicode_to_utf8(str, wstrFileOut);
-   }
-   return b;
-}
+   
+   /*
+    bool file_system::FullPath(string &str, const char * lpszFileIn)
+    {
+    
+    //if(::ex1::file_system::FullPath(str, lpszFileIn))
+    // return true;
+    
+    if(gen::str::begins_ci(lpszFileIn, "http://"))
+    {
+    
+    str = lpszFileIn;
+    
+    return true;
+    
+    }
+    else if(gen::str::begins_ci(lpszFileIn, "https://"))
+    {
+    
+    str = lpszFileIn;
+    
+    return true;
+    
+    }
+    wstring wstrFileIn;
+    wstrFileIn = gen::international::utf8_to_unicode(lpszFileIn);
+    wstring wstrFileOut;
+    bool b = vfxFullPath(wstrFileOut.alloc(MAX_PATH * 8), wstrFileIn) != FALSE;
+    if(b)
+    {
+    gen::international::unicode_to_utf8(str, wstrFileOut);
+    }
+    return b;
+    }
+    
+    
+    
+    UINT file_system::GetFileName(const char * lpszPathName, string & str)
+    {
+    
+    str = System.file().name_(lpszPathName);
+    
+    return str.get_length();
+    
+    }
+    
+    void file_system::GetModuleShortFileName(HINSTANCE hInst, string & strShortName)
+    {
+    vfxGetModuleShortFileName(hInst, strShortName);
+    }
+    
+    */
+   
+} // namespace mac
 
 
-UINT WinFileSystem::GetFileName(const char * lpszPathName, string & str)
-{
-   int nMax = MAX_PATH * 8;
-   wstring wstrPathName;
-   wstrPathName = gen::international::utf8_to_unicode(lpszPathName);
-   wstring wstrTitle;
-   UINT user = vfxGetFileName(wstrPathName, wstrTitle.alloc(nMax), nMax);
-   str = gen::international::unicode_to_utf8(wstrTitle);
-   return user;
-}
-
-void WinFileSystem::GetModuleShortFileName(HINSTANCE hInst, string & strShortName)
-{
-   vfxGetModuleShortFileName(hInst, strShortName);
-}
