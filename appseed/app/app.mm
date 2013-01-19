25e5f016de0a71be42a043af64513e99909145ac
ca2 <camilo@ca2.email>
2013-01-19 15:05:38 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy  Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

---
+++ appseed/app/app.mm
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  app
+//
+//  Created by Camilo Sasuke Tsumanuma on 1/19/13.
+//  Copyright (c) 2013 Camilo Sasuke Tsumanuma. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+extern int32_t __mac_main(int32_t argc, char * argv[]);
+
+
+int main(int argc, char *argv[])
+{
+   
+   return __mac_main(argc, argv);
+   
+}
+
+
+
+
