eb3917442fd8f5aee4b505138aa379b0257eb16f
ca2 <camilo@ca2.email>
2013-01-19 16:56:54 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy  Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

---
+++ os/mac_crypt.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+namespace mac
+{
+
+
+   class CLASS_DECL_mac crypt :
+      virtual public ::ca4::crypt
+   {
+   public:
+
+
+      crypt(::ca::application * papp);
+      virtual ~crypt();
+
+
+      virtual bool encrypt(primitive::memory & storageEncrypt, const primitive::memory & storageDecrypt, const char * pszSalt);
+      virtual bool decrypt(primitive::memory & storageDecrypt, const primitive::memory & storageEncrypt, const char * pszSalt);
+
+
+      virtual string get_crypt_key_file_path();
+
+
+
+   };
+
+   
+} // namespace mac
+
+
+
+
+
