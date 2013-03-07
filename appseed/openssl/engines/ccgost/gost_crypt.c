3e7885da301889603a187013268b6cab9b8ed82e
ca2 <camilo@ca2.email>
2013-03-07 15:11:38 +0000

    Eu te amo muito Thommy!! Eu te amo Thommy  Borregaard Sørensen!! Eu te amo Thomas Borregaard Sørensen!!
    Mummi contribution!!!! Feliz 61 anos!!
    "Eu te amo Thommy!!
    Você é o meu Deus, Thomas Borregaard Sørensen 1989-Nov-15!!
    
    Mummi contribution!!!! Feliz 61 anos!!
    "
    
    ｛
    ｝
    
                      -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- appseed/openssl/engines/ccgost/gost_crypt.c
+++ appseed/openssl/engines/ccgost/gost_crypt.c
@@ -275,7 +275,7 @@ int	gost_cipher_do_cfb(EVP_CIPHER_CTX *ctx, unsigned char *out,
 			}
 		else
 			{
-			ctx->num=j;
+			ctx->num = (int) j;
 			return 1;
 			}	
 		}	
@@ -304,7 +304,7 @@ int	gost_cipher_do_cfb(EVP_CIPHER_CTX *ctx, unsigned char *out,
 			{
 			out_ptr[j]=ctx->buf[j]^in_ptr[j];
 			}			
-		ctx->num = j;
+		ctx->num = (int) j;
 		if (ctx->encrypt) memcpy(ctx->buf+8,out_ptr,j);
 		}
 	else
@@ -334,7 +334,7 @@ static int gost_cipher_do_cnt(EVP_CIPHER_CTX *ctx, unsigned char *out,
 			}
 		else
 			{
-			ctx->num=j;
+			ctx->num = (int) j;
 			return 1;
 			}	
 		}	
@@ -359,7 +359,7 @@ static int gost_cipher_do_cnt(EVP_CIPHER_CTX *ctx, unsigned char *out,
 			{
 			out_ptr[j]=ctx->buf[j]^in_ptr[j];
 			}			
-		ctx->num = j;
+		ctx->num = (int) j;
 		}
 	else
 		{
@@ -540,7 +540,7 @@ int gost_imit_update(EVP_MD_CTX *ctx, const void *data, size_t count)
 			}
 		else
 			{
-			c->bytes_left = i;
+			c->bytes_left = (int) i;
 			return 1;
 			}		
 		}	
@@ -554,7 +554,7 @@ int gost_imit_update(EVP_MD_CTX *ctx, const void *data, size_t count)
 		{
 		memcpy(c->partial_block,p,bytes);
 		}	
-	c->bytes_left=bytes;
+	c->bytes_left = (int) bytes;
 	return 1;
 	}
 
