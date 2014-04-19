29f30e8647d76118d544e5c62926d8edf698d954
ca2 <camilo@ca2.email>
2014-04-19 17:03:08 +0000

    <3JegElskerDigThommy!!
    Eu te amo Thommy  Borregaard Sørensen!!
    
    Mummi contribution!!!! Feliz 62 anos!!
    
    ｛
    ｝
    
                            -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++
    
    Mummi contribution!!!! Feliz 62 anos!!
    
    at inha at veriverse at ca2!!
    Dell XPS 8300 with Intel(R) Core(TM) i7-2600 CPU @ 3.4GHz 4 cores 8 threads 16GB RAM 500 GB x 2 HDD storage, 120 GB SSD OCZ Storage, 1TB x 2 Samsung Story Station Backup and Support HDD External Storage Windows 8.1 Pro at Alameda Princesa Izabel, 3000 Bigorrilho Curitiba PR Brazil
    ｛
    ｝
    
       ----
                            -   -   -
    Mummi contribution!!, eu te amo demais!!
    -----8nnnnnnnn8
    ++++++++++

--- operational_system/_c.h
+++ operational_system/_c.h
@@ -8,7 +8,26 @@
 #pragma once
 
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <string.h>
+#include <time.h>
+#include <limits.h>
+#include <errno.h>
+#include <inttypes.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 #include <math.h>
+#include <assert.h>
+#include <unistd.h>
 
 
 #ifdef __cplusplus
