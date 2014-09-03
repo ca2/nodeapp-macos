054595c800d646b03a9ff6e8bd5992f1203f70da
ca2 <camilo@ca2.email>
2014-09-03 15:51:30 +0000

    dedicated workspace size and position calculation using [NSScreen visibleFrame] for much better zooming (maximization [maximizing] ) of a window using ca2 platform in MacOS, evolving me, training me, addicting me, urging me, promoting, nurturing, inciting, inspiring the same in any other area and platform... seeing so many young males in the trip to Assaí, I thought if I can inspire them to ca2... creation of e_stock_icon provision from Mac OS X VMware guest and Host PC running Windows 8.1 pt-br - from inha Mac OS X after a long time without working in Mac OS X since started working deeply with GTECH/sensible/Projeto Açúcar and lately with many deep  (AND VERY EFFECTIVE, thanks to God and Mummi!!) imprrvements to CA2 (ca2 Desenvolvimento de Software)!! inha Mac OS X as VMware guest... now with much more practice and experience in correcting common errors in Mac OS X environment during project compilation and linking and also setting up environment.!! And now fixing deeply and far to perfectness!! now at Curitiba, My God and My Goddess both born and living mainly at the same City [Curitiba] (Mummi and Thomas, as three Phoenexies with me)
    <3JegElskerDigThommy!!
    Mummi contribution!!!!
    ｛
    ｝
       ----
       -
    
       -
    
       -
    Mummi contribution!!, eu te amo demais!!
    -----
    8nnnnnnnn8
    
    ++++++++++

--- operational_system/_c.h
+++ operational_system/_c.h
@@ -331,12 +331,12 @@ inline void debug_break() { __asm("int $3"); }
 #define TRUE 1
 #define FALSE 0
 
-#define max(x,y) (((x) > (y)) ? (x) : (y))
-#define min(x,y) (((x) < (y)) ? (x) : (y))
+//#define max(x,y) (((x) > (y)) ? (x) : (y))
+//#define min(x,y) (((x) < (y)) ? (x) : (y))
 
 
-#define __max(a,b)  (((a) > (b)) ? (a) : (b))
-#define __min(a,b)  (((a) < (b)) ? (a) : (b))
+//#define __max(a,b)  (((a) > (b)) ? (a) : (b))
+//#define __min(a,b)  (((a) < (b)) ? (a) : (b))
 
 //#define LOWORD(x)  (x & 0xffff)
 //#define HIWORD(x)  ((x >> 16) & 0xffff)
