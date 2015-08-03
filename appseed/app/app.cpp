388613e7bf3d86b552583e79495dc512fb6492fe
ca2 <camilo@ca2.email>
2015-08-03 15:17:13 +0000

    Twitch.tv/abnercoimbre contribution!! Livecoding.tv pals and pallies and palms (and shades of gray) (oh god the dicotomy, besides I prefer certain color, a very specific tone... only one, a single DNA...) (mumu, Programdeleted, CurelJ, ghastlyccc, banks, tuyyui, mariaNYC, CBAlmeida, rodrigocaldeira, Deleted, Xvolt78 and all others learning, watching or just making "quorum" ...) contribution!! Mac OS porting effort contribution!! Generic Mac OS X Yosemite at Dell XPS 8300 with Xcode contribution!! HMH Twitch contribution (handmade_hero... and audiencemade_too)!! Jeito Moleque contribution!! Djavan Senior contribution!!
    <3TBS
    Thank you Bilbo and Lucinho!!

--- appseed/app/app.cpp
+++ appseed/app/app.cpp
@@ -7,10 +7,8 @@
 //
 
 #include "app.h"
-#include "aura/aura/node/macos/macos.h"
+#include "aura/aura/os/macos/macos.h"
 
-
-extern int32_t __mac_main(int32_t argc, char * argv[]);
 
 
 int main(int argc, char *argv[])
@@ -20,11 +18,24 @@ int main(int argc, char *argv[])
    
    processid=getpid();
    
-   printf("%d\n", processid);
+   fwrite(&processid, sizeof(pid_t), 1, stdout);
+   
+   if(!defer_core_init())
+   {
+
+      printf("app.exe failed miserably : !defer_core_init() < C lang... progamming is all about coding, verbs, and good (or perfect and precise, doesn't need enoughly \"good\", I let you choose... how powerful I think I am...) language...");
+      
+      return -1;
+      
+   }
+   
+   ::output_debug_string("*****app.exe going on : defer_core_init() < C lang... progamming is all about coding, verbs, and good (or perfect and precise, doesn't need enoughly \"good\", I let you choose... how powerful I think I am...) language...");
    
    bool bInstall = false;
    
    bool bUninstall = false;
+   
+   output_debug_string("app.exe main()\n\n\n\n\n");
    
    for(index i = 0; i < argc; i++)
    {
@@ -42,7 +53,13 @@ int main(int argc, char *argv[])
          
       }
       
+      output_debug_string("arg(" + ::str::from(i) + ") => " + string(argv[i]));
+      output_debug_string("\n");
+      
    }
+   
+   output_debug_string("\n\n\n\n\n");
+
    
    if(bInstall)
    {
@@ -71,10 +88,14 @@ int main(int argc, char *argv[])
   // fflush(stdout);
 
    
-   if(!defer_core_init())
-      return -1;
    
-   int iResult = __mac_main(argc, argv);
+   main_param * v = new main_param;
+   
+   v->argc = argc;
+   
+   v->argv = argv;
+   
+   int iResult = __run_system_main_param(v);
    
    try
    {
@@ -92,105 +113,105 @@ int main(int argc, char *argv[])
 }
 
 
-int32_t ca2_main(HINSTANCE hInstance, HINSTANCE hPrevInstance, const char * lpCmdLine, int32_t nCmdShow)
-{
-   
-   
-
-   try
-   {
-      
-      ::core::system * psystem = new ::core::system();
-      
-      ASSERT(hPrevInstance == NULL);
-      
-      int32_t nReturnCode = 0;
-      
-      ::macos::main_init_data * pinitmaindata  = new ::macos::main_init_data;
-      
-      pinitmaindata->m_hInstance             = hInstance;
-      
-      pinitmaindata->m_hPrevInstance         = hPrevInstance;
-      
-      pinitmaindata->m_strCommandLine        = lpCmdLine;
-
-      pinitmaindata->m_nCmdShow              = nCmdShow;
-      
-      psystem->init_main_data(pinitmaindata);
-
-      bool bOk = false;
-      
-      try
-      {
-      
-         if(psystem->pre_run())
-         {
-            
-            bOk = true;
-         
-         }
-         
-      }
-      catch(...)
-      {
-         
-      }
-      
-      try
-      {
-         
-         if(!bOk)
-         {
-      
-            if(psystem->m_iReturnCode == 0)
-            {
-         
-               return -1;
-            
-            }
-            
-            return psystem->m_iReturnCode;
-            
-         }
-         
-         
-      }
-      catch(...)
-      {
-         
-         return -1;
-         
-      }
- 
-      
-      nReturnCode = psystem->main();
-      
-      try
-      {
-         
-         delete psystem;
-         
-      }
-      catch(...)
-      {
-      
-      }
-      
-      psystem = NULL;
-      
-      return nReturnCode;
-      
-   }
-   catch(...)
-   {
-      
-   }
-   
-   return -1;
-   
-}
-
-
-
-
+//int32_t ca2_main(HINSTANCE hInstance, HINSTANCE hPrevInstance, const char * lpCmdLine, int32_t nCmdShow)
+//{
+//   
+//   
+//
+//   try
+//   {
+//      
+//      ::core::system * psystem = new ::core::system();
+//      
+//      ASSERT(hPrevInstance == NULL);
+//      
+//      int32_t nReturnCode = 0;
+//      
+//      ::macos::main_init_data * pinitmaindata  = new ::macos::main_init_data;
+//      
+//      pinitmaindata->m_hInstance             = hInstance;
+//      
+//      pinitmaindata->m_hPrevInstance         = hPrevInstance;
+//      
+//      pinitmaindata->m_strCommandLine        = lpCmdLine;
+//
+//      pinitmaindata->m_nCmdShow              = nCmdShow;
+//      
+//      psystem->init_main_data(pinitmaindata);
+//
+//      bool bOk = false;
+//      
+//      try
+//      {
+//      
+//         if(psystem->pre_run())
+//         {
+//            
+//            bOk = true;
+//         
+//         }
+//         
+//      }
+//      catch(...)
+//      {
+//         
+//      }
+//      
+//      try
+//      {
+//         
+//         if(!bOk)
+//         {
+//      
+//            if(psystem->m_iReturnCode == 0)
+//            {
+//         
+//               return -1;
+//            
+//            }
+//            
+//            return psystem->m_iReturnCode;
+//            
+//         }
+//         
+//         
+//      }
+//      catch(...)
+//      {
+//         
+//         return -1;
+//         
+//      }
+// 
+//      
+//      nReturnCode = psystem->main();
+//      
+//      try
+//      {
+//         
+//         delete psystem;
+//         
+//      }
+//      catch(...)
+//      {
+//      
+//      }
+//      
+//      psystem = NULL;
+//      
+//      return nReturnCode;
+//      
+//   }
+//   catch(...)
+//   {
+//      
+//   }
+//   
+//   return -1;
+//   
+//}
+//
+//
+//
+//
 
