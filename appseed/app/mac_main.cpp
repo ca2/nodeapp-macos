cde0ed3d8ac830a527981290d6b317436c7d25a9
Thomas Sørensen <ts@jumdum.com>
2016-03-17 20:20:06 +0000

    LCTV contribution!! Mummi contribution!! catherinacortega contribution!!
    Thank you Carlos!!
    
    Thank you Mummi!!
    
    Thank you Carol!!
    
    from inha-macos!!
    i7 2600 16GB 1333MHz DDR3 2x500GB HDD
    at /macos/ folder at inha-macos at veriverse!!

--- appseed/app/mac_main.cpp
+++ appseed/app/mac_main.cpp
@@ -18,99 +18,118 @@ int main(int argc, char *argv[])
    
    processid=getpid();
    
-   fwrite(&processid, sizeof(pid_t), 1, stdout);
+   printf("\napplication_process_id=%d\n", processid);
    
    if(!defer_core_init())
    {
 
-      printf("app.exe failed miserably : !defer_core_init() < C lang... progamming is all about coding, verbs, and good (or perfect and precise, doesn't need enoughly \"good\", I let you choose... how powerful I think I am...) language...");
+//      printf("app.exe failed miserably : !defer_core_init() < C lang... progamming is all about coding, verbs, and good (or perfect and precise, doesn't need enoughly \"good\", I let you choose... how powerful I think I am...) language...");
       
       return -1;
       
    }
-   
-   ::output_debug_string("*****app.exe going on : defer_core_init() < C lang... progamming is all about coding, verbs, and good (or perfect and precise, doesn't need enoughly \"good\", I let you choose... how powerful I think I am...) language...");
-   
-   bool bInstall = false;
-   
-   bool bUninstall = false;
-   
-   output_debug_string("app.exe main()\n\n\n\n\n");
-   
-   for(index i = 0; i < argc; i++)
-   {
 
-      if(strcmp(argv[i], "install") == 0)
+
+      
+      int iResult = base_main_command_line(":", argc, argv);
+      
+      try
       {
          
-         bInstall = true;
+         defer_core_term();
          
       }
-      else if(strcmp(argv[i], "install") == 0)
+      catch(...)
       {
-         
-         bUninstall = true;
          
       }
       
-      output_debug_string("arg(" + ::str::from(i) + ") => " + string(argv[i]));
-      output_debug_string("\n");
+      return iResult;
       
    }
-   
-   output_debug_string("\n\n\n\n\n");
 
-   
-   if(bInstall)
-   {
-     
-//      if(setuid(0) != 0)
-  //    {
+////   ::output_debug_string("*****app.exe going on : defer_core_init() < C lang... progamming is all about coding, verbs, and good (or perfect and precise, doesn't need enoughly \"good\", I let you choose... how powerful I think I am...) language...");
+//   
+//   bool bInstall = false;
+//   
+//   bool bUninstall = false;
+//   
+//   output_debug_string("app.exe main()\n\n\n\n\n");
+//   
+//   for(index i = 0; i < argc; i++)
+//   {
 //
-  //       return -3;
-    //
-      //}
-
-   }
-   else if(bUninstall)
-   {
-    
-//      if(setuid(0) != 0)
-  //    {
-    //
-      //   return -3;
-        //
-      //}
-      
-   }
-   
-//   fwrite(&processid,sizeof(processid),1,stdout);
-  // fflush(stdout);
-
-   
-   
-   main_param * v = new main_param;
-   
-   v->argc = argc;
-   
-   v->argv = argv;
-   
-   int iResult = __run_system_main_param(v);
-   
-   try
-   {
-      
-      defer_core_term();
-      
-   }
-   catch(...)
-   {
-      
-   }
-
-   return iResult;
-   
-}
+//      if(strcmp(argv[i], "install") == 0)
+//      {
+//         
+//         bInstall = true;
+//         
+//      }
+//      else if(strcmp(argv[i], "install") == 0)
+//      {
+//         
+//         bUninstall = true;
+//         
+//      }
+//      
+//      output_debug_string("arg(" + ::str::from(i) + ") => " + string(argv[i]));
+//      output_debug_string("\n");
+//      
+//   }
+//   
+//   output_debug_string("\n\n\n\n\n");
+//
+//   
+//   if(bInstall)
+//   {
+//     
+////      if(setuid(0) != 0)
+//  //    {
+////
+//  //       return -3;
+//    //
+//      //}
+//
+//   }
+//   else if(bUninstall)
+//   {
+//    
+////      if(setuid(0) != 0)
+//  //    {
+//    //
+//      //   return -3;
+//        //
+//      //}
+//      
+//   }
+//   
+////   fwrite(&processid,sizeof(processid),1,stdout);
+//  // fflush(stdout);
+//
+//   
+//   
+//   main_param * v = new main_param;
+//   
+//   v->argc = argc;
+//   
+//   v->argv = argv;
+//   
+//   int iResult = __run_system_main_param(v);
+//   
+//   try
+//   {
+//      
+//      defer_core_term();
+//      
+//   }
+//   catch(...)
+//   {
+//      
+//   }
+//
+//   return iResult;
+//   
+//}
 
 
 //int32_t ca2_main(HINSTANCE hInstance, HINSTANCE hPrevInstance, const char * lpCmdLine, int32_t nCmdShow)
