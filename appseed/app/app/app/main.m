e22fbc8ceac640d1aa65a849eca3241df74e0499
Thomas Sørensen <ts@jumdum.com>
2016-03-19 11:56:26 +0000

    veriverse contribution!! Mummi contribution!! LCTV contribution!! drmjg contribution!! michaelphp contribution!! from inha-macos!! i7 2600 16GB 1333MHz DDR3 2x500GB HDD at /macos/ folder at inha-macos at veriverse!!

---
+++ appseed/app/app/app/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  app
+//
+//  Created by Thomas Borregaard Sørensen on 3/18/16.
+//  Copyright © 2016 ca2 Desenvolvimento de Software Ltda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+   @autoreleasepool {
+       // insert code here...
+       NSLog(@"Hello, World!");
+   }
+    return 0;
+}
